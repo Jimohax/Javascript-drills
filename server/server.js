da14226b657fcf8c25b23044bef5e5e1c96e909a
Jamie Ohax <jimohax@gmail.com>
2023-03-14 15:17:44 +0100

    APIs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import  bodyParser from "body-parser";
+// import dotenv from "dotenv";
+
 
 const app = express();
+// dotenv.config();
+app.use(express.json());
+
+
+const courses = [
+    
+        {id:1, name: "course1"},
+        {id:2, name: "course2"},
+        {id:3, name: "course3"},   
+]
 
 
 
@@ -10,9 +22,21 @@ app.get("/", (req, res)=>{
     res.send("Hello, my geee!");
 });
 
+app.get("/api/courses", (req, res)=>{
+    res.send(courses);
+});
 
-
-
-app.listen(3000, ()=>{
-    console.log("Server started on port 3000 BOSS");
+app.get("/api/courses/:id", (req, res)=>{
+    const course = courses.find(c=>c.id === parseInt(req.params.id));
+    if(!course){
+        res.send("course doesnt exist")
+    }else{
+        res.send(course);
+    }
 })
+
+
+const port = process.env.PORT || 5000 ;
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port} BOSS`);
+});
