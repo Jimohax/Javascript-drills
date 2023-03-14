23578ecc5e8f4fb0c94d690a9febb0b6c22662b9
Jamie Ohax <jimohax@gmail.com>
2023-03-14 10:57:53 +0100

    express server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello, my geee!");
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000 BOSS");
+})
